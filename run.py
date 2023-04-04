2358626c3c3c8d74f40192e052fc82bbc54bc597
PatSvedberg <pat.svedberg@gmail.com>
2023-04-04 12:23:04 +0000

    Rewrite print text and code clean up

--- run.py
+++ run.py
@@ -36,13 +36,15 @@ def insert_weight():
     '''
     # Ask the user to enter their weight and convert it to an integer
     while True:
-        weight = input("Enter your current weight: ")
+        weight = input("Enter your current weight in kilograms: ")
         try:
             weight = int(weight)
             break
         except ValueError:
             # If the user enters an invalid input, print an error message
-            print("Error: Weight must be an integer.")
+            print(
+                "Error: Weight must be an integer. A number without decimals"
+            )
 
     return weight
 
@@ -69,23 +71,25 @@ def activity_pick(worksheet, username, weight):
     '''
     Pick activity. Jog or swim using the keys 1 and 2
     '''
-    print(f"Welcome '{username}' what did you do today?\n")  
+    print(f"Welcome {username} what did you do today?\n")  
     print("1 - Jogging")
     print("2 - Swimming\n")  
 
     try:
         activity = int(
-            input("Enter activity number (1 for Jogging, 2 for Swimming): ")
+            input("Enter activity number (1 or 2): ")
         )
     except ValueError:
         print("Invalid input, please enter a number.")
         return activity_pick(worksheet, username, weight)
 
     if activity == 1:
-        jogging(worksheet, weight)  # Pass the worksheet object to jog function
+        # Pass the worksheet object to jog function
+        jogging(worksheet, weight)
         calculate_jog_value(worksheet, weight)
     elif activity == 2:
-        swimming(worksheet, weight)  # Pass the worksheet object to swim function
+        # Pass the worksheet object to swim function
+        swimming(worksheet, weight)
         calculate_swim_value(worksheet, weight)
     else:
         print("Invalid input, please select 1 or 2.")
@@ -96,9 +100,12 @@ def jogging(worksheet, weight):
     Function for jogging activity
     '''
     jogging_time = input("How many minutes did you jog?: ")
-    column_values = worksheet.col_values(2) # Get all values in 2nd column of worksheet
-    next_row = len(column_values) + 1  # Find next empty row in the 2nd column
-    worksheet.update_cell(next_row, 2, jogging_time)  # Add jogging time to the next empty slot in the second column
+    # Get all values in 2nd column of worksheet
+    column_values = worksheet.col_values(2)
+    # Find next empty row in the 2nd column
+    next_row = len(column_values) + 1
+    # Add jogging time to the next empty slot in the second column
+    worksheet.update_cell(next_row, 2, jogging_time)
     calculate_jog_value(worksheet, weight)
 
 
@@ -107,9 +114,12 @@ def swimming(worksheet, weight):
     Function for swimming activity
     '''
     swimming_distance = input("How many minutes did you swim?: ")
-    column_values = worksheet.col_values(4)  # Get all the values in the third column of the worksheet
-    next_row = len(column_values) + 1  # Find next empty row in the 4th column
-    worksheet.update_cell(next_row, 4, swimming_distance)  # Add swimming distance to the next empty slot in the third column
+    # Get all the values in the third column of the worksheet
+    column_values = worksheet.col_values(4)
+    # Find next empty row in the 4th column
+    next_row = len(column_values) + 1
+    # Add swimming distance to the next empty slot in the third column
+    worksheet.update_cell(next_row, 4, swimming_distance)
     calculate_swim_value(worksheet, weight)
 
 
